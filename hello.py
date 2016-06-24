bf4fa81860d562584da7add85c5eb4ba9bad47e4
Ignacio Sanchez Gines <nacho@geardome.com>
2016-06-24 23:53:24 +0200

    hello world python + docker

---
+++ hello.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+import socket
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Hello World from %s!' % socket.gethostname()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
