1b97bdb3d0a4285c0b1fea1eb6b272c45eb08bfa
Ignacio Sanchez Gines <nacho@geardome.com>
2017-02-15 12:52:35 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World from host \"%s\".' % socket.gethostname()
+    return 'Hello World from host \"%s\".\n' % socket.gethostname()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
